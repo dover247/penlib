2865d39e8dedb8e9aa3564c3c6a3cc8936a554f6
george <secondclass325@gmail.com>
2016-10-28 11:50:08 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='penlib',
+        author="George Sauceda",
+        author_email="secondclass325@gmail.com",
+        url="https://github.com/dover247/penlib/",
+        version='0.1.1',
+        description='A Python Penetration Package',
+        packages=find_packages(),
+        install_requires=['bs4', 'requests'])
