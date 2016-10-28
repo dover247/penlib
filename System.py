45119349ef9332c5a58223aeaeb48101a530a756
george <secondclass325@gmail.com>
2016-10-27 22:17:57 -0500

    Update System.py

--- System.py
+++ System.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from platform import *
 
 
@@ -21,27 +20,3 @@ class System(object):
             hostname and processor of the system'''
     def get(self):
         return self.platform, self.architecture, self.hostname, self.processor
-=======
-from platform import *
-
-
-class System(object):
-    '''Attempts To Find System Hardware, Hostname and Operating System.'''
-    def __init__(self):
-        self.platform = platform()
-        self.architecture = architecture()[0]
-        self.hostname = node()
-        self.processor = processor()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, error, traceback):
-        if error:
-            print(error)
-
-    ''' returns the platform the archiecture
-            hostname and processor of the system'''
-    def get(self):
-        return self.platform, self.architecture, self.hostname, self.processor
->>>>>>> origin/master
