45a0e135f613bece7150035a3a7c6419cb1513c4
george <secondclass325@gmail.com>
2016-10-27 22:21:48 -0500

    Update Seeker.py

--- Seeker.py
+++ Seeker.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 
 
@@ -24,30 +23,3 @@ class Seeker(object):
                 if filename.lower() in Filename.lower():
                     print(os.path.join(path, Filename))
                     self.count.append(Filename)
-=======
-import os
-
-
-class Seeker(object):
-    '''loops files given path and matches a particular string.'''
-    def __init__(self):
-        self.count = []
-
-    def __len__(self):
-        return len(self.count)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, error, traceback):
-        if error:
-            print(error)
-
-    '''searches for files using a path and file name'''
-    def search_file(self, filename, path):
-        for path, directories, files in os.walk(path):
-            for Filename in files:
-                if filename.lower() in Filename.lower():
-                    print(os.path.join(path, Filename))
-                    self.count.append(Filename)
->>>>>>> origin/master
