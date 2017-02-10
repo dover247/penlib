bbabc3c81ca2d52a1a0676132532cda74449e544
thescriptkid <secondclass325@gmail.com>
2017-02-10 12:32:52 -0600

    renamed files, updated version number

--- penlib/__init__.py
+++ penlib/__init__.py
@@ -1,9 +1,8 @@
 from . import postexploitation
 from . import reconnaissance
 from . import exploitation
-from . import scanning
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __author__ = 'George Sauceda'
 __license__ = 'GPL'
 __title__ = 'penlib'
