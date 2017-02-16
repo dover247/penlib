c8bf0535707b9cf6f746e45f11572e0faa200325
thescriptkid <secondclass325@gmail.com>
2017-02-16 00:35:59 -0600

    Added More Documentation, pep8 compliant

--- penlib/reconnaissance.py
+++ penlib/reconnaissance.py
@@ -1,27 +1,25 @@
-import requests
+"""Reconnaissance."""
+
 from bs4 import BeautifulSoup
+import requests
 
 
 class Scrape(object):
-    '''Fecthes a Specific page and finds tags.'''
+    """Fecthes a Specific page and finds tags."""
+
     def __init__(self, page):
+        """Start Parser."""
         self.tags = []
         self.page = requests.get(page)
         self.content = self.page.content
         self.href_parser = BeautifulSoup(self.content, "html.parser")
 
     def __len__(self):
+        """Return the amount of tags."""
         return len(self.tags)
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, error, traceback):
-        if error:
-            print(error)
-
     def scrape(self, tag):
-        '''scrapes the chosen tag.'''
+        """Scrape the chosen tag."""
         for tags in self.href_parser.find_all(tag):
             self.tags.append(tags)
         return self.tags
